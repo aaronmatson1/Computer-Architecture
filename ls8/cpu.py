f49364f7b061c770f8ce2af54ce27c5ddd4bb6d1
matsonaaron1 <matsonaaron1@gmail.com>
2020-12-13 19:30:48 -0700

    Implement AND, OR, XOR, NOT ops

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -16,6 +16,10 @@ CMP = 0b10100111
 JMP = 0b01010100
 JEQ = 0b01010101
 JNE = 0b01010110
+AND = 0b10101000
+OR  = 0b10101010
+XOR = 0b10101011
+NOT = 0b01101001
 
 class CPU:
     """Main CPU class."""
@@ -55,6 +59,11 @@ class CPU:
         self.branchtable[JMP] = self.execute_JMP
         self.branchtable[JEQ] = self.execute_JEQ
         self.branchtable[JNE] = self.execute_JNE
+        self.branchtable[JNE] = self.execute_JNE
+        self.branchtable[AND] = self.execute_AND
+        self.branchtable[OR] = self.execute_OR
+        self.branchtable[XOR] = self.execute_XOR
+        self.branchtable[NOT] = self.execute_NOT
 
         # Property wrapper for stack pointers
         @property
@@ -235,3 +244,15 @@ class CPU:
                     self.execute_JMP()
                 else:
                     self.pc += self.instruction_size
+
+            def execute_AND(self):
+                self.reg[sel.operand_a] &= self.reg[self.operand_b]
+
+            def execute_OR(self):
+                self.reg[self.operand_a] |= self.reg[self.operand_b]
+
+            def execute_XOR(self):
+                self.reg[self.operand_a] ^= self.reg[self.operand_b]
+
+            def execute_NOT(self):
+                self.reg[self.operand_a] = ~self.reg[self.operand_a]
